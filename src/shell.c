f4f8cf2c5ebcf8985f183def3f88d6eabe629d67
Omer Banay <omer.banay@gmail.com>
2020-07-03 15:35:11 -0700

    Use the project's errors module

--- src/shell.c
+++ src/shell.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
 #include <stdlib.h>
 #include "shell.h"
+#include "errors.h"
 
 #define LINE_MAXIMUM_SIZE 1024
 
-void error(char * message) {
-    fprintf(stderr, "%s: %s\n", message, strerror(errno));
-}
+
 char * read_line() {
     char* line_buffer = malloc(LINE_MAXIMUM_SIZE * sizeof(char));
     if (!line_buffer) {
-        error("allocation error");
+        shellock_error(ERROR_MEMORY);
     }
 }
 void shell_loop() {
     int result_status;
 
-    printf("Starting shell starting");
+    printf("Starting shell starting\n");
     do {
         printf("> ");
-        
+        read_line();
     }
     while(result_status == 0);
 }
--- src/shell.h
+++ src/shell.h
@@ -1,3 +1,2 @@
-void error(char * message);
 char * read_line();
 void shell_loop();
